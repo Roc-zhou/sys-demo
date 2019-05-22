7a92fb9224e6f160fa3bce0c7451a31af1719357
zhouhaipeng <zhouhaipeng@coldlar.com>
2019-05-22 18:00:47 +0800

    update

--- docs/sys.static/js/3.f2fcc2470558b71612c1.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{iTWN:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r={render:function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"bb"},[this._v("BBBB")])},staticRenderFns:[]};var u=n("VU/8")({beforeRouteEnter:function(t,e,n){return n(function(t){})},name:"bb",data:function(){return{}}},r,!1,function(t){n("u8SV")},"data-v-783c37da",null);e.default=u.exports},u8SV:function(t,e){}});
-//# sourceMappingURL=3.f2fcc2470558b71612c1.js.map
